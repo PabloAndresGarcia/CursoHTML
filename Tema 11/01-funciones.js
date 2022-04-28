ba60af759bc6e59083b3fefc00e717de01a2a114
PabloAndresGarcia <garciasjs@gmail.com>
2022-04-28 10:33:18 -0300

    Commit 28/4

---
+++ Tema 11/01-funciones.js
@@ -0,0 +1,14 @@
+function displayName()
+{
+    document.write ("Pablo Andres García");
+}
+function sayHello()
+{
+    document.write("Hola, qué tal?");
+}
+function displayScore()
+
+{
+    var playerScore=2300
+    document.write ("<p> La puntuación del jugador es: "+ playerScore + "</p>");
+}
